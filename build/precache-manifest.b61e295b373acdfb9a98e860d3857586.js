a70e764fe52cedabe8285d543ca2efb6979077fb
Callum Hutchinson <callumhutchie@hotmail.co.uk>
2020-05-15 13:26:21 +0100

    react app

---
+++ build/precache-manifest.b61e295b373acdfb9a98e860d3857586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a346a8f0810f7e385f09958b5e23764b",
+    "url": "/React-Guild-Wars-Skill-Template/index.html"
+  },
+  {
+    "revision": "b969279d842fcf1b2912",
+    "url": "/React-Guild-Wars-Skill-Template/static/css/main.bf41395b.chunk.css"
+  },
+  {
+    "revision": "e08e7eca030107957202",
+    "url": "/React-Guild-Wars-Skill-Template/static/js/2.34659799.chunk.js"
+  },
+  {
+    "revision": "af83badb6478bb975648c8cc5c86c873",
+    "url": "/React-Guild-Wars-Skill-Template/static/js/2.34659799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b969279d842fcf1b2912",
+    "url": "/React-Guild-Wars-Skill-Template/static/js/main.eb4d5572.chunk.js"
+  },
+  {
+    "revision": "916550532d4bf1e03035",
+    "url": "/React-Guild-Wars-Skill-Template/static/js/runtime-main.476b4e8f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Guild-Wars-Skill-Template/static/media/logo.5d5d9eef.svg"
+  }
+]);
